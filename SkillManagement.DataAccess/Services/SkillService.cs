09b6e06fae8f8677a5f7501c7b0463ded2e6bf92
Mohamed Ramadan <MRamadan@integrant.com>
2018-08-29 15:57:16 +0200

    Adding Skill API with it service

---
+++ SkillManagement.DataAccess/Services/SkillService.cs
@@ -0,0 +1,38 @@
+﻿using SkillManagement.DataAccess.Entities;
+using SkillManagement.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillManagement.DataAccess.Services
+{
+    public class SkillService : ISkillService
+    {
+        public int AddSkill(Skill skill)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteSkill(Skill skill)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Skill> GetAllSkills()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Skill GetSkillById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSkill(Skill skill)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
