a73333a487837814c0b6bf08faf0ee2c7268ecb9
Mohamed Ramadan <MRamadan@integrant.com>
2018-08-29 13:08:11 +0200

    Adding DB project to the solution

---
+++ SkillManagement.DataBase/dbo/Tables/SkillCategories.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[SkillCategories] (
+    [Id]   INT            NOT NULL,
+    [Name] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_SkillCategories] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
